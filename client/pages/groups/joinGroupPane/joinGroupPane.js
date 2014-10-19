763732dd02780ac1b9cb780518f9794f7d09c2e5
--global user.name "Craig Cheney <--global user.email "ccheney@mit.edu>
2014-10-19 16:26:03 -0400

    Wired up Join Group
    Adds groupId to user profile, and adds userId to group members

--- client/pages/groups/joinGroupPane/joinGroupPane.js
+++ client/pages/groups/joinGroupPane/joinGroupPane.js
@@ -1,15 +1,35 @@
 Template.joinGroupPane.helpers({
 	groups: function () {
-		return Groups.find({ _id: { $not:{$in: Meteor.user().profile.groups}}});
-		// return Groups.find({});
+		if (Meteor.user() !==undefined) {
+			return Groups.find({ _id: { $not:{$in: Meteor.user().profile.groups}}});
+		}
 	}
 });
 
-Template.joinGroupTile.helpers({
-	memberPlur: function (length) {
-		if(length >1){
-			return "members";
+Template.joinGroupPane.events({
+	'click .joinGroupBtn': function (event, template) {
+		reply = confirm("Are you sure you want to join " + this.name + "?");
+		if(reply){
+			console.log("joined");
+
+			//add user id to group list
+			Groups.update({_id:this._id}, {
+				$push:{
+					members:Meteor.userId()
+				}
+			});
+
+			//add group to user profile 
+			Meteor.users.update({_id:Meteor.userId()}, {
+				$push:{
+					"profile.groups":this._id
+				}
+			});
+
+
 		}
-		return "member";
+		else{
+			console.log("didn't join");
+		}
 	}
 });
